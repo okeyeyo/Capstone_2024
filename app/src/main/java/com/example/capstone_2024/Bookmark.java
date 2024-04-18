5f1a0e3f1414590ed8e5e341b7282fd91e04b8c9
fkqlaus <fkqlaus@naver.com>
2024-04-18 15:07:18 +0900

    인옥이 요리 관련 xml 가져오기

---
+++ app/src/main/java/com/example/capstone_2024/Bookmark.java
@@ -0,0 +1,33 @@
+package com.example.capstone_2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Bookmark extends AppCompatActivity {
+
+    Button homebtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bookmark);
+
+        homebtn = findViewById(R.id.home);
+
+        homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeActivity();
+            }
+        });
+    }
+    public void openHomeActivity() {
+        Intent intent = new Intent(Bookmark.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
+
