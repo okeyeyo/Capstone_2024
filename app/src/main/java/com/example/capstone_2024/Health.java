cbdf00ba7f3b5a4effd9de13e07ef3d8ca9ead9e
fkqlaus <fkqlaus@naver.com>
2024-05-29 19:43:48 +0900

    헬스 xml 타이머 텍스트뷰로 있던거 크로노로 바꾸고 버튼 기능 자바 코드 추가

--- app/src/main/java/com/example/capstone_2024/Health.java
+++ app/src/main/java/com/example/capstone_2024/Health.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageButton;
 
 
 public class Health extends AppCompatActivity {
@@ -14,15 +17,34 @@ public class Health extends AppCompatActivity {
     Button strechbtn;
     Button recvrbtn;
     Button musclebtn;
-
+    ImageButton startbtn, resetbtn;
+    Chronometer chronometer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.health);
 
+        startbtn = findViewById(R.id._button1);
+        resetbtn = findViewById(R.id._button2);
+
         homebtn = findViewById(R.id.home);
         strechbtn = findViewById(R.id.streching);
         musclebtn = findViewById(R.id.muscle);
+        chronometer = (Chronometer) findViewById(R.id.timer);
+
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.start();
+            }
+        });
+
+        resetbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
 
         homebtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,14 +67,16 @@ public class Health extends AppCompatActivity {
             }
         });
 
+
     }
+
     public void openHomeActivity() {
         Intent intent = new Intent(Health.this, MainActivity.class);
         startActivity(intent);
     }
 
     public void openStrechingActivity() {
-        Intent intent = new Intent(Health.this, Streching.class);
+        Intent intent = new Intent(getApplicationContext(), Streching.class);
         startActivity(intent);
     }
 
