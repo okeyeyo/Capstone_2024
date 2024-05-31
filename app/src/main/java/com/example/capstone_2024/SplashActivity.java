9e8dd5008323a6d6693b2371a5aa0a6d9ed01342
okeyeyo <clskfk1234@naver.com>
2024-05-31 17:21:40 +0900

    로딩 화면

--- app/src/main/java/com/example/capstone_2024/SplashActivity.java
+++ app/src/main/java/com/example/capstone_2024/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.capstone_2024;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //일정 시간 지연 후 실행하기 위한 코드
+        Handler hd = new Handler();
+        hd.postDelayed(new splashHandler(), 3000); //posetDelayed를 통해 2초 뒤에 작동하도록 설정
+    }
+
+    private class splashHandler implements Runnable {
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
     }
 }
