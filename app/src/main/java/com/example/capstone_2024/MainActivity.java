5c90685af5a1a9be8c7c4b75e25ebccda3f276b9
okeyeyo <clskfk1234@naver.com>
2024-06-01 13:12:47 +0900

    상단 툴바 완성

--- app/src/main/java/com/example/capstone_2024/MainActivity.java
+++ app/src/main/java/com/example/capstone_2024/MainActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button homebtn;
+    ImageButton homebtn;
     Button cookbtn;
     Button healthbtn;
 
