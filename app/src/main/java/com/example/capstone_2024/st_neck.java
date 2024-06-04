fdccf1556213336e6827ca6c4a0a3c3441773716
fkqlaus <fkqlaus@naver.com>
2024-06-04 16:25:18 +0900

    스트레칭 관련 자바파일 추가

---
+++ app/src/main/java/com/example/capstone_2024/st_neck.java
@@ -0,0 +1,66 @@
+package com.example.capstone_2024;
+
+import static android.widget.Toast.makeText;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class st_neck extends AppCompatActivity {
+    TextView txtResult;
+    public android.widget.Button buttonShowDialog,back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.streching_list_neck);
+
+        buttonShowDialog = findViewById(R.id.button2);
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Streching.class);
+                startActivity(intent);
+
+            }
+        });
+        buttonShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showDialog();
+            }
+        });
+
+
+    }
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(st_neck.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.st_popup, null);
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        // 다이얼로그 크기 조정
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        Button buttonOk = dialogView.findViewById(R.id.button_ok);
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+}
