05a22085010faca528edda12c82564d12859bd67
fkqlaus <fkqlaus@naver.com>
2024-06-08 01:23:33 +0900

    근력운동 부분 튕김 문제 해결

--- app/src/main/java/com/example/capstone_2024/muscle_shoulder.java
+++ app/src/main/java/com/example/capstone_2024/muscle_shoulder.java
@@ -1,6 +1,7 @@
 package com.example.capstone_2024;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
 import android.content.Intent;
@@ -17,7 +18,8 @@ import java.util.Stack;
 
 public class muscle_shoulder extends AppCompatActivity {
     boolean display = true;
-    android.widget.Button[] btn = new Button[7];
+    //android.widget.Button[] btn = new Button[7];
+    androidx.appcompat.widget.AppCompatButton[] btn = new AppCompatButton[7];
 
 
     Stack<Integer> layoutStack = new Stack<>();
@@ -26,6 +28,7 @@ public class muscle_shoulder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.muscle_main_shoulder);
+
 
         Muscle.listbtnset(this);
         Muscle.clickrebtn(this);
